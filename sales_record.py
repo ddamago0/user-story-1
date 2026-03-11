79594d2b2da7ef821c12724b5e36e093b5a75512
Daniel Martinez <ddamago0@gmail.com>
2026-03-11 12:31:37 -0500

    defining sales record variables

---
+++ sales_record.py
@@ -0,0 +1,5 @@
+def sales_record():
+    product = input ("Enter the product name: ")
+    price = float (input("Enter the price of the product: "))
+    quantity = int (input("Enter the quantity sold"))
+

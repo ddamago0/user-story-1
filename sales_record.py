98a757724c9964c94f867e73f15c551602c229ea
Daniel Martinez <ddamago0@gmail.com>
2026-03-11 12:42:33 -0500

    create sale dictionary and return sale record

--- sales_record.py
+++ sales_record.py
@@ -1,5 +1,11 @@
 def sales_record():
     product = input ("Enter the product name: ")
     price = float (input("Enter the price of the product: "))
-    quantity = int (input("Enter the quantity sold"))
+    quantity = int (input("Enter the quantity sold: "))
 
+    sale = {
+        "product": product,
+        "price": price,
+        "quantity": quantity
+    }
+    return sale

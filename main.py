eebab220b49a4875e32a257566daebb5830abb0b
Daniel Martinez <ddamago0@gmail.com>
2026-03-11 14:35:35 -0500

    connect total calculations with main program

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from sales_record import sales_record
+from total_calculations import calculate_totals
 sales_list = []
 
 continue_registering = True
@@ -10,3 +11,5 @@ while continue_registering:
     answer = input("Do you want to register another sale? (yes/no): ")
     if answer.lower() != "yes":
         continue_registering = False
+
+product_totals, total_revenue = calculate_totals(sales_list)
